461d5425e3cc4de5a291d9148d65314c7d3a94c9
sandrasoza <ssozazambrano@luc.edu>
2026-07-09 12:14:07 -0500

    Made changes on Count visitor so the primitive shapes return 1, wrappers delegate, and group adds all children. All unit tests on Count passing.

--- app/src/main/java/edu/luc/etl/cs313/android/shapes/model/Count.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/model/Count.java
@@ -10,41 +10,49 @@ public class Count implements Visitor<Integer> {
 
     @Override
     public Integer onPolygon(final Polygon p) {
-        return -1;
+        return 1;
     }
 
     @Override
     public Integer onCircle(final Circle c) {
-        return -1;
+        return 1;
     }
 
     @Override
     public Integer onGroup(final Group g) {
-        return -1;
+        int total = 0;
+        for(final Shape shape : g.getShapes()) {
+            total += shape.accept(this);
+        }
+        return total;
     }
 
     @Override
     public Integer onRectangle(final Rectangle q) {
-        return -1;
+        return 1;
     }
 
     @Override
     public Integer onOutline(final Outline o) {
-        return -1;
+
+        return o.getShape().accept(this);
     }
 
     @Override
     public Integer onFill(final Fill c) {
-        return -1;
+
+        return c.getShape().accept(this);
     }
 
     @Override
     public Integer onLocation(final Location l) {
-        return -1;
+
+        return l.getShape().accept(this);
     }
 
     @Override
     public Integer onStrokeColor(final StrokeColor c) {
-        return -1;
+
+        return c.getShape().accept(this);
     }
 }
